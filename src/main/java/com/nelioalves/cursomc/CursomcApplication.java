1f58282a8f0d7432f160da2385160ed23ef7a6c4
VitorAndrade0721 <vitorhenriquealt@gmail.com>
2021-03-07 22:31:19 -0300

    Pagincao com parametros opcionais na requisicao

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -64,6 +64,11 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		Categoria cat1 =  new Categoria(null, "Informatica");
 		Categoria cat2 =  new Categoria(null, "Escritorio");
+		Categoria cat3 =  new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 =  new Categoria(null, "Eletronicos");
+		Categoria cat5 =  new Categoria(null, "Jardinagem");
+		Categoria cat6 =  new Categoria(null, "Decoração");
+		Categoria cat7 =  new Categoria(null, "Perfumaria");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -120,7 +125,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getItens().addAll(Arrays.asList(ip3));
 		p3.getItens().addAll(Arrays.asList(ip2));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat2,cat3,cat4,cat5,cat6,cat7));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -5,12 +5,16 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
 import com.nelioalves.cursomc.services.Exceptions.DataIntegrityException;
 import com.nelioalves.cursomc.services.Exceptions.ObjectNotFoundException;
+
 
 @Service	
 public class CategoriaService {
@@ -52,6 +56,12 @@ public class CategoriaService {
 	public List<Categoria> findAll(){
 		
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
 	}
 	
 	
