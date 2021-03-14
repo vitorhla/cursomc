be1e004b38564b83ce8a120ecfe8434381e07bb7
VitorAndrade0721 <vitorhenriquealt@gmail.com>
2021-03-13 21:01:35 -0300

    Subtotal e total do pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -35,6 +35,13 @@ public class ItemPedido implements Serializable{
 		this.preco = preco;
 	}
 
+	
+	public double getSubTotal() {
+		
+		return (preco -  desconto) * quantidade;
+		
+	}
+	
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
