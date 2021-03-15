63e259590e1c04f5c0f138b29c591333334d0869
VitorAndrade0721 <vitorhenriquealt@gmail.com>
2021-03-15 14:37:08 -0300

    MockEmailService com Logger. Padroes Strategy e Template Method

---
+++ src/main/java/com/nelioalves/cursomc/services/EmailService.java
@@ -0,0 +1,13 @@
+package com.nelioalves.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.nelioalves.cursomc.domain.Pedido;
+
+public interface EmailService {
+
+	
+void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+}
--- src/main/java/com/nelioalves/cursomc/services/PedidoService.java
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -37,6 +37,8 @@ public class PedidoService {
 	@Autowired
 	private ClienteService clienteService;
 	
+	@Autowired
+	private EmailService emailService;
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -70,7 +72,7 @@ public class PedidoService {
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
 		
-	System.out.println(obj);
+	emailService.sendOrderConfirmationEmail(obj);
 		
 		return obj;
 	}
