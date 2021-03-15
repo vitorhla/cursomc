dc1a6e07cd98f9e0aae8a52cf4a716f0fcdcced3
VitorAndrade0721 <vitorhenriquealt@gmail.com>
2021-03-15 19:11:11 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/nelioalves/cursomc/config/DevConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.nelioalves.cursomc.services.DBService;
+import com.nelioalves.cursomc.services.EmailService;
+import com.nelioalves.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -31,5 +33,12 @@ public class DevConfig {
 		
 		return true;
 	}
+
+	@Bean
+	public EmailService emailService() {
+		
+		return new SmtpEmailService();
+	}
+	
 
 }
--- src/main/java/com/nelioalves/cursomc/services/DBService.java
+++ src/main/java/com/nelioalves/cursomc/services/DBService.java
@@ -84,7 +84,7 @@ public class DBService {
 		Cidade c2 =  new Cidade (null, "São Paulo", est2);
 		Cidade c3 =  new Cidade (null, "Campinas", est2);
 		
-		Cliente cli1  = new Cliente (null, "Maria Silva", "maria@gmail.com","36378912377", TipoCliente.PESSOAFISICA );
+		Cliente cli1  = new Cliente (null, "Maria Silva", "vitorhenriquealt@gmail.com","36378912377", TipoCliente.PESSOAFISICA );
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 		
 		Endereco e1 =  new Endereco(null,"Rua Flores","300", "Apto 303", "Jardim", "38220834", cli1,c1);
