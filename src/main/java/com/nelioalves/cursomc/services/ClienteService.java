3cefd6206eb9b19b80031bb2ba64f12bdc0afe9c
VitorAndrade0721 <vitorhenriquealt@gmail.com>
2021-03-20 18:34:19 -0300

    salvando url da imagem no cliente

--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -130,8 +130,19 @@ public class ClienteService {
 	}
 	
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
+
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
+		URI uri =  s3Service.uploadFile(multipartFile);
+		Optional<Cliente> cli = repo.findById(user.getId());
+		cli.orElse(null).setImageUrl(uri.toString());
+		repo.save(cli.orElse(null));
+		return uri;
 		
-		return s3Service.uploadFile(multipartFile);
+		
 		
 	}
 	
